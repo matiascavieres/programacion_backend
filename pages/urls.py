4ad69c1b88b303111eb884da975c0b2971c661ea
matias cavieres <imatiascavieres@outlook.com>
2022-09-15 01:43:38 -0400

    Cambios Varios logrin/Register/

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView , LoginPageView, Sign_UpPageView
+from .views import HomePageView, AboutPageView , LoginPageView, RegisterPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("login/", LoginPageView.as_view(), name="login"),
-    path("sign_up/", Sign_UpPageView.as_view(), name="sign_up"),
+    path("register/", RegisterPageView.as_view(), name="register"),
 ]
